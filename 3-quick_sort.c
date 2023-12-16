6abfccd0e8a24dede72931650445d27fb2fa2b4d
mohamed kamal <kmido9729@gmail.com>
2023-12-16 02:03:50 +0200

    quiksort BIG O fix bugs

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -4,12 +4,13 @@
  * swap_func - Swaps two integers in an array
  * @a: Pointer to the first integer
  * @b: Pointer to the second integer
+ * Return: 0
  */
 void swap_func(int *a, int *b)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 /**
@@ -20,24 +21,29 @@ void swap_func(int *a, int *b)
  * @size: Size of the array
  * Return: Index of the pivot element
  */
-int partition_func(int *array, int down, int up, size_t size)
+size_t partition_func(int *array, int down, int up, size_t size)
 {
-	int pivot = array[up];
-	int i = down - 1;
-	int j;
+    int pivot = array[up];
+    size_t i = down - 1;
+    size_t j;
 
-	for (j = down; j < up; j++)
-	{
-		if (array[j] < pivot)
-		{
-			i++;
-			swap_func(&array[i], &array[j]);
-		}
-	}
+    for (j = down; j < (size_t)up; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+            if (i != j)
+            {
+                swap_func(&array[i], &array[j]);
+                print_array(array, size);
+            }
+        }
+    }
 
-	swap_func(&array[i + 1], &array[up]);
-	print_array(array, size);
-	return (i + 1);
+    swap_func(&array[i + 1], &array[up]);
+    if (array[i + 1] != array[up])
+        print_array(array, size);
+    return (i + 1);
 }
 
 /**
@@ -46,33 +52,39 @@ int partition_func(int *array, int down, int up, size_t size)
  * @down: Start index of the partition
  * @up: End index of the partition
  * @size: Size of the array
+ * Return: 0
  */
 void q_sort_recursive(int *array, int down, int up, size_t size)
 {
-	if (down < up)
-	{
-		int pivot_index = partition_func(array, down, up, size);
+    size_t pivot_ind;
 
-		if (pivot_index != down)
-		{
-			q_sort_recursive(array, down, pivot_index - 1, size);
-		}
+    if (down < up)
+    {
+        pivot_ind = partition_func(array, down, up, size);
 
-		q_sort_recursive(array, pivot_index + 1, up, size);
-	}
+        if (pivot_ind > 1)
+        {
+            q_sort_recursive(array, down, pivot_ind - 1, size);
+        }
+        if (up > 1)
+        {
+            q_sort_recursive(array, pivot_ind + 1, up, size);
+        }
+    }
 }
 
 /**
  * quick_sort - Sorts an array of integers in ascending order using Quick sort
  * @array: Array to be sorted
  * @size: Size of the array
+ * Return: 0
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-	{
-		return;
-	}
+    if (array == NULL || size < 2)
+    {
+        return;
+    }
 
-	q_sort_recursive(array, 0, size - 1, size);
+    q_sort_recursive(array, 0, size - 1, size);
 }
