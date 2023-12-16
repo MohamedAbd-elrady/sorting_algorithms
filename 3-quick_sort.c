f9ff75f859c9ca76abe687d9a173c8eda7da0e09
mohamed kamal <kmido9729@gmail.com>
2023-12-16 02:04:41 +0200

    quiksort BIG O fix bugs

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,9 +8,9 @@
  */
 void swap_func(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
@@ -23,27 +23,27 @@ void swap_func(int *a, int *b)
  */
 size_t partition_func(int *array, int down, int up, size_t size)
 {
-    int pivot = array[up];
-    size_t i = down - 1;
-    size_t j;
+	int pivot = array[up];
+	size_t i = down - 1;
+	size_t j;
 
-    for (j = down; j < (size_t)up; j++)
-    {
-        if (array[j] < pivot)
-        {
-            i++;
-            if (i != j)
-            {
-                swap_func(&array[i], &array[j]);
-                print_array(array, size);
-            }
-        }
-    }
+	for (j = down; j < (size_t)up; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap_func(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
 
-    swap_func(&array[i + 1], &array[up]);
-    if (array[i + 1] != array[up])
-        print_array(array, size);
-    return (i + 1);
+	swap_func(&array[i + 1], &array[up]);
+	if (array[i + 1] != array[up])
+		print_array(array, size);
+	return (i + 1);
 }
 
 /**
@@ -56,21 +56,21 @@ size_t partition_func(int *array, int down, int up, size_t size)
  */
 void q_sort_recursive(int *array, int down, int up, size_t size)
 {
-    size_t pivot_ind;
+	size_t pivot_ind;
 
-    if (down < up)
-    {
-        pivot_ind = partition_func(array, down, up, size);
+	if (down < up)
+	{
+		pivot_ind = partition_func(array, down, up, size);
 
-        if (pivot_ind > 1)
-        {
-            q_sort_recursive(array, down, pivot_ind - 1, size);
-        }
-        if (up > 1)
-        {
-            q_sort_recursive(array, pivot_ind + 1, up, size);
-        }
-    }
+		if (pivot_ind > 1)
+		{
+			q_sort_recursive(array, down, pivot_ind - 1, size);
+		}
+		if (up > 1)
+		{
+			q_sort_recursive(array, pivot_ind + 1, up, size);
+		}
+	}
 }
 
 /**
@@ -81,10 +81,10 @@ void q_sort_recursive(int *array, int down, int up, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-    {
-        return;
-    }
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
 
-    q_sort_recursive(array, 0, size - 1, size);
+	q_sort_recursive(array, 0, size - 1, size);
 }
