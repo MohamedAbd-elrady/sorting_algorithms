20761b9d7beeea11f67e69c74587ea424d7be018
MohamedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-12-21 16:04:24 +0200

    Fix betty

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -8,44 +8,44 @@
  */
 void counting_sort(int *array, size_t size)
 {
-    int *count, *output;
-    int max = 0, i;
+	int *count, *output;
+	int max = 0, i;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    /* Find the maximum value in the array */
-    for (i = 0; i < (int)size; i++)
-    {
-        if (max < array[i])
-            max = array[i];
-    }
-    count = malloc(sizeof(int) * (max + 1));
-    output = malloc(sizeof(int) * size);
+	/* Find the maximum value in the array */
+	for (i = 0; i < (int)size; i++)
+	{
+		if (max < array[i])
+			max = array[i];
+	}
+	count = malloc(sizeof(int) * (max + 1));
+	output = malloc(sizeof(int) * size);
 
-    if (!count || !output)
-    {
-        free(count);
-        free(output);
-        return;
-    }
-    for (i = 0; i <= max; i++)
-        count[i] = 0;
-    for (i = 0; i < (int)size; i++)
-        count[array[i]]++;
-    for (i = 1; i <= max; i++)
-        count[i] += count[i - 1];
+	if (!count || !output)
+	{
+		free(count);
+		free(output);
+		return;
+	}
+	for (i = 0; i <= max; i++)
+		count[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		count[array[i]]++;
+	for (i = 1; i <= max; i++)
+		count[i] += count[i - 1];
 
-    print_array(count, max + 1);
+	print_array(count, max + 1);
 
-    for (i = (int)size - 1; i >= 0; i--)
-    {
-        output[count[array[i]] - 1] = array[i];
-        count[array[i]]--;
-    }
-    for (i = 0; i < (int)size; i++)
-        array[i] = output[i];
+	for (i = (int)size - 1; i >= 0; i--)
+	{
+		output[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
+	}
+	for (i = 0; i < (int)size; i++)
+		array[i] = output[i];
 
-    free(count);
-    free(output);
+	free(count);
+	free(output);
 }
